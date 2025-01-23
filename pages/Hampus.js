c13c382965fe188acf6734c5e3bee0ba94475777
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-01-23 11:18:53 +0100

    Updated Hampus.js

--- pages/Hampus.js
+++ pages/Hampus.js
@@ -1,13 +1,6 @@
 import ButtonComponent from "@/components/ButtonComponent";
 import { useState } from "react";
 
-// import { GoogleGenerativeAI } from "@google/generative-ai";
-
-// const apiKey = process.env.NEXT_PUBLIC_API_KEY
-
-// const genAI = new GoogleGenerativeAI(apiKey);
-// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
 export default function Hampus() {
   // const [category, setCategory] = useState("");
   const [promt, setPromt] = useState("");
