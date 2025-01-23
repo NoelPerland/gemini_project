aa416d88571e7512c7ea6b8b0aae0a321739e2a5
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-01-23 10:17:33 +0100

    Updated Hampus page and buttonComponent

--- components/ButtonComponent.js
+++ components/ButtonComponent.js
@@ -1,7 +1,7 @@
 export default function ButtonComponent({ title, func }) {
   return (
     <button
-      onClick={func}
+      onClick={() => func(title)}
       value={title}
       className="btn bg-white text-lg text-gray-900  hover:text-white"
     >
