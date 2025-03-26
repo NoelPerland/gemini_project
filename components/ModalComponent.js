34a42800b291412efd2090e7461dc64a44bc787a
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-03-26 14:11:27 +0100

    added favorites to localstorage

--- components/ModalComponent.js
+++ components/ModalComponent.js
@@ -1,34 +1,30 @@
-export default function ModalComponent({ showModal, history, recoverHistory }) {
+export default function ModalComponent({
+  showModal,
+  array,
+  recover,
+  title,
+  fallback,
+  modalType,
+}) {
+  console.log(modalType);
   return (
     <div className="flex flex-col rounded-lg bg-white shadow-lg text-gray-800 w-3/4 lg:w-2/4 text-center p-5 lg:p-10">
-      <h3 className="font-title text-2xl lg:text-7xl mb-5 lg:mb-16">
-        History:
-      </h3>
+      <h3 className="font-title text-2xl lg:text-7xl mb-5 lg:mb-16">{title}</h3>
       <ul className="flex flex-wrap font-title text-gray-500 flex-col text-xl lg:text-4xl text-center gap-5 ">
-        {(() => {
-          const items = [];
-          if (history.length > 1) {
-            for (let i = 1; i < 6; i++) {
-              if (history[i]) {
-                items.push(
-                  <li
-                    className="w-full hover:text-purple-500 hover:cursor-pointer"
-                    onClick={() => {
-                      recoverHistory(history.length - i);
-                    }}
-                    key={i}
-                  >
-                    {history[history.length - i]}
-                  </li>
-                );
-              }
-            }
-          } else if (history.length == 1) {
-            items.push(<li>Nothing here! make a search...</li>);
-          }
-
-          return items;
-        })()}
+        {modalType === "history" && (
+          <HistoryComponent
+            fallback={fallback}
+            array={array}
+            recover={recover}
+          />
+        )}
+        {modalType === "favorites" && (
+          <FavoriteComponent
+            fallback={fallback}
+            array={array}
+            recover={recover}
+          />
+        )}
       </ul>
       <div className="modal-action">
         {/* if there is a button in form, it will close the modal */}
@@ -41,4 +37,54 @@ export default function ModalComponent({ showModal, history, recoverHistory }) {
       </div>
     </div>
   );
+}
+
+export function HistoryComponent({ array, recover, fallback }) {
+  const items = [];
+  if (array.length > 1) {
+    for (let i = 1; i < 6; i++) {
+      if (array[i]) {
+        items.push(
+          <li
+            className="w-full hover:text-purple-500 hover:cursor-pointer"
+            onClick={() => {
+              {
+                recover(array.length - i);
+              }
+            }}
+            key={i}
+          >
+            {array[array.length - i]}
+          </li>
+        );
+      }
+    }
+  } else if (array.length == 1) {
+    items.push(<li key="0">{fallback}</li>);
+  }
+
+  return items;
+}
+
+export function FavoriteComponent({ array, fallback, recover }) {
+  const items = [];
+  if (array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      items.push(
+        <li
+          className="w-full hover:text-purple-500 hover:cursor-pointer"
+          onClick={() => {
+            {
+              recover(i);
+            }
+          }}
+        >
+          {array[i].name}
+        </li>
+      );
+    }
+  } else {
+    items.push(<li key="0">{fallback}</li>);
+  }
+  return items;
 }
--- pages/Hampus.js
+++ pages/Hampus.js
@@ -4,7 +4,6 @@ import { model } from "@/util/ai";
 //Components
 import ButtonComponent from "@/components/ButtonComponent";
 import ModalComponent from "@/components/ModalComponent";
-import FavoriteModal from "@/components/FavoriteModal";
 
 //Icons:
 import { CgArrowDownO } from "react-icons/cg";
@@ -12,12 +11,12 @@ import { CgArrowUpO } from "react-icons/cg";
 import { CgCloseO } from "react-icons/cg";
 import { IoReloadCircleOutline } from "react-icons/io5";
 import { FaHistory } from "react-icons/fa";
-import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder } from "react-icons/md";
 
 export default function Hampus() {
   //states
   const [promt, setPromt] = useState("");
-  const [inputTitle, setInputTitle] = useState("Food Categories");
+  const [inputTitle, setInputTitle] = useState("Food Categories or Meals");
   const [favoritePopup, setFavoritePopup] = useState("hidden");
 
   //states []
@@ -113,6 +112,18 @@ export default function Hampus() {
     setShowRecipe(true);
   }
 
+  function recoverFavorite(index) {
+    //hide favorite modal
+    setShowFavorites(false);
+    //apply loader to generate button
+    setIsLoading(true);
+    setAnswer(favorites[index]);
+    //remove loader from generate button
+    setIsLoading(false);
+    //show recipe
+    setShowRecipe(true);
+  }
+
   //open history modal
   function openModal() {
     if (!showModal) {
@@ -122,16 +133,24 @@ export default function Hampus() {
     }
   }
 
+  function closeFavorite() {
+    if (!showFavorites) {
+      setShowFavorites(true);
+    } else {
+      setShowFavorites(false);
+    }
+  }
+
   //get localstarage on page load
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem("history"));
+    const data = JSON.parse(localStorage.getItem("history")) || [];
     setHistory(data);
 
-    const data2 = JSON.parse(localStorage.getItem("answerHistory"));
+    const data2 = JSON.parse(localStorage.getItem("answerHistory")) || [];
     setAnswerHistory(data2);
 
-    /* const data3 = JSON.parse(localStorage.getItem("favorites"));
-    setFavorites(data3); */
+    const data3 = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(data3);
   }, []);
 
   //update local storange
@@ -141,6 +160,12 @@ export default function Hampus() {
       localStorage.setItem("answerHistory", JSON.stringify(answerHistory));
     }
   }, [history]);
+
+  useEffect(() => {
+    if (favorites.length > 0) {
+      localStorage.setItem("favorites", JSON.stringify(history));
+    }
+  }, [favorites]);
 
   //Update Favorites
 
@@ -163,123 +188,143 @@ export default function Hampus() {
   return (
     <>
       <div className="flex justify-end bg-white pr-5 pt-5">
-        <MdFavorite
+        <MdFavoriteBorder
           className="text-gray-800 text-5xl"
           onClick={() => setShowFavorites(!showFavorites)}
         />
       </div>
-      <FavoriteModal
-        favorites={favorites}
-        setAnswer={setAnswer}
-        show={showFavorites}
-      />
-      <div className="flex flex-col font-sans bg-white to-gray-950 p-5 lg:p-10 lg:px-20  py-16 lg:py-40 gap-10">
+
+      {showFavorites && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <ModalComponent
+            showModal={closeFavorite}
+            array={favorites}
+            recover={recoverFavorite}
+            title="Favorites"
+            fallback="Add favorites to display them here!"
+            modalType="favorites"
+          />
+        </div>
+      )}
+
+      <div className="flex flex-col font-sans bg-white to-gray-950 p-5 lg:p-10 lg:px-20  py-11 lg:py-40 gap-10">
         <div className="flex flex-col items-stretch text-center">
           <h1 className="text-xl font-bold text-center text-gray-400 pb-2">
             Recipes
           </h1>
-          <h2 className="text-center font-title text-4xl lg:text-6xl text-gray-800 font-bold mb-16 lg:mb-40 drop-shadow-lg">
+          <h2 className="text-center font-title text-4xl lg:text-8xl text-gray-800 font-bold mb-16 lg:mb-40 drop-shadow-lg">
             What are you craving for?
           </h2>
           <div className="font-title">
             <input
               onChange={addCategory}
               type="text"
-              className="bg-gray-100 w-3/5 lg:w-2/5 h-20 p-5 rounded-l-lg text-gray-800 text-lg lg:text-xl outline-none"
+              className="bg-gray-100 w-3/5 lg:w-2/5 h-20 lg:h-32 p-5 rounded-l-lg text-gray-800 text-lg lg:text-2xl outline-none"
               placeholder={inputTitle}
             ></input>
             <button
               onClick={sendPromt}
-              className="btn-xs lg:btn-lg rounded-r-lg font-title h-20 bg-gray-800 text-lg lg:text-xl text-white border-none hover:bg-gray-900"
+              className="btn-xs lg:btn-lg rounded-r-lg font-title h-20 lg:h-32 bg-gray-800 text-lg lg:text-xl text-white border-none hover:bg-gray-900"
             >
               {isLoading ? "Loading..." : "Generate"}
             </button>
           </div>
         </div>
 
-        <div className="flex flex-col gap-5 mt-16 lg:mt-20">
-          <div className="flex flex-wrap justify-center gap-5">
-            <ButtonComponent title="Comfort Food" func={applyPromt} />
-            <ButtonComponent title="Healthy Options" func={applyPromt} />
-            <ButtonComponent title="Sweet Treats" func={applyPromt} />
+        {!showRecipe && (
+          <>
+            <div className="flex flex-col gap-5 mt-16 lg:mt-40">
+              <div className="flex flex-wrap justify-center gap-5">
+                <ButtonComponent title="Comfort Food" func={applyPromt} />
+                <ButtonComponent title="Healthy Options" func={applyPromt} />
+                <ButtonComponent title="Sweet Treats" func={applyPromt} />
 
-            {show && (
-              <>
-                <ButtonComponent title="Savory Snacks" func={applyPromt} />
-                <ButtonComponent title="Fast Food" func={applyPromt} />
-                <ButtonComponent
-                  title="International Cuisines"
-                  func={applyPromt}
-                />
-                <ButtonComponent title="Vegetarian" func={applyPromt} />
-                <ButtonComponent title="Breakfast" func={applyPromt} />
-                <ButtonComponent title="Seafood" func={applyPromt} />
-                <ButtonComponent title="Light Bites" func={applyPromt} />
-                <ButtonComponent title="Cheat Meals" func={applyPromt} />
-                <ButtonComponent
-                  title="Late-Night Cravings"
-                  func={applyPromt}
-                />
+                {show && (
+                  <>
+                    <ButtonComponent title="Savory Snacks" func={applyPromt} />
+                    <ButtonComponent title="Fast Food" func={applyPromt} />
+                    <ButtonComponent
+                      title="International Cuisines"
+                      func={applyPromt}
+                    />
+                    <ButtonComponent title="Vegetarian" func={applyPromt} />
+                    <ButtonComponent title="Breakfast" func={applyPromt} />
+                    <ButtonComponent title="Seafood" func={applyPromt} />
+                    <ButtonComponent title="Light Bites" func={applyPromt} />
+                    <ButtonComponent title="Cheat Meals" func={applyPromt} />
+                    <ButtonComponent
+                      title="Late-Night Cravings"
+                      func={applyPromt}
+                    />
 
-                <ButtonComponent title="Spicy Foods" func={applyPromt} />
-                <ButtonComponent title="Grilled & BBQ" func={applyPromt} />
-                <ButtonComponent title="Drinks & Smoothies" func={applyPromt} />
-                <ButtonComponent title="Fine Dining" func={applyPromt} />
-              </>
-            )}
-          </div>
-        </div>
+                    <ButtonComponent title="Spicy Foods" func={applyPromt} />
+                    <ButtonComponent title="Grilled & BBQ" func={applyPromt} />
+                    <ButtonComponent
+                      title="Drinks & Smoothies"
+                      func={applyPromt}
+                    />
+                    <ButtonComponent title="Fine Dining" func={applyPromt} />
+                  </>
+                )}
+              </div>
+            </div>
 
-        <hr />
+            <hr />
 
-        <div className="flex justify-center gap-5">
-          {!show && (
-            <button
-              className="flex font-title text-sm justify-center items-center gap-2 border-2 border-gray-500 rounded-full p-5  text-gray-500 hover:text-gray-800"
-              onClick={showMore}
-            >
-              <p>Show More</p>
-              <CgArrowDownO className="text-xl" />
-            </button>
-          )}
+            <div className="flex justify-center gap-5">
+              {!show && (
+                <button
+                  className="flex font-title text-sm justify-center items-center gap-2 border-2 border-gray-500 rounded-full p-5  text-gray-500 hover:text-gray-800"
+                  onClick={showMore}
+                >
+                  <p>Show More</p>
+                  <CgArrowDownO className="text-xl" />
+                </button>
+              )}
 
-          {show && (
-            <button
-              className="flex font-title text-sm justify-center items-center gap-2 border-2 border-gray-500 rounded-full p-5 text-gray-500 hover:text-gray-800"
-              onClick={showMore}
-            >
-              <p>Show Less</p>
-              <CgArrowUpO className="text-xl" />
-            </button>
-          )}
+              {show && (
+                <button
+                  className="flex font-title text-sm justify-center items-center gap-2 border-2 border-gray-500 rounded-full p-5 text-gray-500 hover:text-gray-800"
+                  onClick={showMore}
+                >
+                  <p>Show Less</p>
+                  <CgArrowUpO className="text-xl" />
+                </button>
+              )}
 
-          <button
-            className="flex font-title text-sm justify-center items-center gap-2 border-2 border-gray-500 rounded-full p-5 text-gray-500 hover:text-gray-800"
-            onClick={openModal}
-          >
-            <p>History</p>
-            <FaHistory className="text-xl" />
-          </button>
-        </div>
+              <button
+                className="flex font-title text-sm justify-center items-center gap-2 border-2 border-gray-500 rounded-full p-5 text-gray-500 hover:text-gray-800"
+                onClick={openModal}
+              >
+                <p>History</p>
+                <FaHistory className="text-xl" />
+              </button>
+            </div>
+          </>
+        )}
         {showModal && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <ModalComponent
               showModal={openModal}
-              history={history}
-              recoverHistory={recoverHistory}
+              array={history}
+              recover={recoverHistory}
+              title="History: "
+              fallback="Nothing here! make a search..."
+              modalType="history"
             />
           </div>
         )}
 
         {showRecipe && (
-          <div className="flex flex-col bg-white text-gray-900 rounded-lg box-border shadow-xl shadow-gray-500">
+          <div className="flex flex-col bg-white text-gray-900 rounded-lg box-border shadow-xl shadow-gray-500 mt-20 lg:mt-32">
             <button
               onClick={() => {
-                sendPromt();
+                setShowRecipe(false);
+                setAnswer([]);
               }}
-              className="flex bg-purple-500 text-white lg:text-base  justify-center text-4xl p-5  hover:bg-purple-800 rounded-t-lg"
+              className="flex w-full text-base bg-purple-500 justify-center lg:text-4xl p-5 hover:bg-purple-800 text-white rounded-t-lg"
             >
-              <IoReloadCircleOutline className="text-xl lg:text-5xl" />
+              <CgCloseO />
             </button>
             <div className="flex justify-center p-5 items-center bg-purple-300">
               <h2 className="flex flex-wrap text-2xl lg:py-5 lg:text-6xl font-title text-center">
@@ -333,14 +378,14 @@ export default function Hampus() {
                 className="flex w-1/2 text-base bg-purple-500 justify-center lg:text-4xl p-5 hover:bg-purple-800 text-white rounded-bl-lg"
                 onClick={updateFavorites}
               >
-                {isFavorite ? <MdFavorite /> : <MdFavoriteBorder />}
+                <MdFavoriteBorder />
               </button>
               <button
                 onClick={() => {
                   setShowRecipe(false);
                   setAnswer([]);
                 }}
-                className="flex w-1/2 text-base bg-purple-500 justify-center lg:text-4xl p-5 hover:bg-purple-800 text-white rounded-br-lg"
+                className="flex w-1/2 text-base bg-purple-600 justify-center lg:text-4xl p-5 hover:bg-purple-800 text-white rounded-br-lg"
               >
                 <CgCloseO />
               </button>
